af1e0a8d61ad0e09c92941859cf8969a112e635b
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-01-27 18:25:42 +0100

    feat: create sign up screan

---
+++ frontend/components/atoms/SignUp.tsx
@@ -0,0 +1,40 @@
+import { Text, View, StyleSheet } from "react-native";
+import Input from "./Input";
+import Email from "./Email";
+import Password from "./Password";
+import Button from "./Button";
+
+function SignIn() {
+  return (
+    <View style={style.container}>
+      <Text style={style.title}>Sign In</Text>
+      <View style={style.containerInput}>
+        <Input icon={<Email />} placeholder="Email" />
+        <Input icon={<Password />} placeholder="Password" />
+      </View>
+      <Button title="Sign Up" onPress={() => {}} />
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F8F8FF",
+    padding: 32,
+    borderWidth: 1,
+    borderRadius: 16,
+  },
+  title: {
+    color: "#B799FF",
+    fontSize: 32,
+  },
+  containerInput: {
+    padding: 10,
+    gap: 24,
+    marginVertical: 24,
+  },
+});
+
+export default SignIn;
